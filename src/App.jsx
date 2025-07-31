62e785dc11a37286cc9ecd2dbeff58f374c087fa
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-07-31 16:00:09 +0700

    khởi tạo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  
+      <h1>Header Component</h1>
+      <p>This is the header section of the app.</p>
     </>
   )
 }
+function Mybutton(){
+  return (
+    <button>Click me</button>
+  )
+}
+ export default  function Myapp (){
+  return (
+    <div>
+     <h1>Toi la thuy day nha</h1>
+     <Header />
+     <Mybutton />
+    </div>
+  )
+ }
 
-export default App
+
